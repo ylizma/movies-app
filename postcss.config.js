8209bd6ffcfcfab89b7f15fae777f756132a35fe
YoussefAmzil <youssefamzil98@gmail.com>
2020-05-01 01:18:41 +0000

    optimizing css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer({
+            add: true,
+            grid: true
+        }),
+        //Only add purgecss in production
+        process.env.NODE_ENV === "production"? purgecss({
+            content: [
+                "./src/**/*.html", 
+                "./src/**/*.vue",
+                "./public/**/*.html"
+            ]
+        }): ""
     ]
-  }
+};
